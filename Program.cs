106784e62e41bb3a18fca087415cbfd60856f474
David Lindberg <david.lindberg@informer.se>
2026-08-25 13:03:15 +0200

    added services into Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
+
+using kanban_lia.Infrastructure.Database;
+using kanban_lia.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<DbConnectionFactory>();
+
+builder.Services.AddScoped<BoardRepository>();
+builder.Services.AddScoped<ColumnRepository>();
+builder.Services.AddScoped<PlacementRepository>();
 
 var app = builder.Build();
 
@@ -16,29 +25,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
-// HIAISIDIASIDOAOSIDOAISDOIASODOASDASODIAHSODIH
-app.MapGet("/weatherforecast", () =>
-{
-
-    var summaries = new[]
-    {   
-        "Freezing", "Bracing", "Chilly", "Cool", "Mildddddd", "Warmaaaaa", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    var forecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast(
-        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        Random.Shared.Next(-20, 55),
-        summaries[Random.Shared.Next(summaries.Length)]
-    )).ToArray();
-
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
