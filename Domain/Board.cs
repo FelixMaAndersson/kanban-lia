75eb379e12828e5c49a1da68dbdf8a3213a9a956
David Lindberg <david.lindberg@informer.se>
2026-08-25 11:26:30 +0200

    Added crud to repositories

--- Domain/Board.cs
+++ Domain/Board.cs
@@ -2,7 +2,7 @@
 {
     public class Board
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Title { get; set; } = string.Empty;
 
         public String TrackedRoots { get; set; } = string.Empty;
--- Infrastructure/Repositories/BoardRepository.cs
+++ Infrastructure/Repositories/BoardRepository.cs
@@ -1,9 +1,72 @@
-﻿
-using Dapper;
+﻿using Dapper;
+using kanban_lia.Infrastructure.Database;
 
 namespace kanban_lia.Infrastructure.Repositories
 {
     public class BoardRepository
     {
+        private readonly DbConnectionFactory _connectionFactory;
+
+        public BoardRepository(DbConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+        public async Task CreateBoardAsync(Domain.Board board)
+        {
+            using var connection = _connectionFactory.CreateConnection();
+            await connection.ExecuteAsync(
+                @"
+                    INSERT INTO Boards (Id, Title, TrackedRoots) 
+                    VALUES (@Id, @Title, @TrackedRoots)",
+                board
+            );
+        }
+
+        public async Task<IEnumerable<Domain.Board>> GetAllBoardsAsync()
+        {
+            using var connection = _connectionFactory.CreateConnection();
+            var boards = await connection.QueryAsync<Domain.Board>(
+                @"
+                    SELECT * FROM Boards"
+            );
+            return boards;
+        }
+
+        public async Task<Domain.Board?> GetBoardByIdAsync(Guid id)
+        {
+            using var connection = _connectionFactory.CreateConnection();
+            var board = await connection.QuerySingleOrDefaultAsync<Domain.Board>(
+                @"
+                    SELECT * FROM Boards 
+                    WHERE Id = @Id",
+                new { Id = id }
+            );
+            return board;
+        }
+
+        public async Task UpdateBoardAsync(Domain.Board board)
+        {
+            using var connection = _connectionFactory.CreateConnection();
+            await connection.ExecuteAsync(
+                @"
+                    UPDATE Boards 
+                    SET Title = @Title, 
+                    TrackedRoots = @TrackedRoots 
+                    WHERE Id = @Id",
+                board
+            );
+        }
+
+        public async Task DeleteBoardAsync(Guid id)
+        {
+            using var connection = _connectionFactory.CreateConnection();
+            await connection.ExecuteAsync(
+                @"
+                    DELETE FROM Boards 
+                    WHERE Id = @Id",
+                new { Id = id }
+            );
+        }
     }
 }
