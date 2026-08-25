261999e9b00f1b45dd4d3e7c24a0585d2193c253
David Lindberg <david.lindberg@informer.se>
2026-08-25 11:58:22 +0200

    kommentar i column domain

--- Domain/Column.cs
+++ Domain/Column.cs
@@ -2,6 +2,7 @@
 {
     public class Column
     {
+        // kanske ska vara en int istället för en guid. p.g.a. ett lågt maxantal
         public Guid Id { get; set;  }
         public string Title { get; set; } = string.Empty;
 
