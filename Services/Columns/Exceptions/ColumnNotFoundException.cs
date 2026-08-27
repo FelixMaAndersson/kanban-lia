75bc0ba18bf38662d85d35d67cdcc634b60f7e3d
David Lindberg <david.lindberg@informer.se>
2026-08-27 14:00:14 +0200

    added exceptions for Column classes

---
+++ Services/Columns/Exceptions/ColumnNotFoundException.cs
@@ -0,0 +1,6 @@
+﻿using kanban_lia.Models.Domain.Columns;
+
+namespace kanban_lia.Services.Columns.Exceptions
+{
+    public class ColumnNotFoundException(ColumnId id) : Exception($"Column with ID '{id.Value}' was not found.");
+}
