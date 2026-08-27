b087f8fcda7eda5dea0eec4e5bb31dabef5bc2ae
felix.andersson <felix.andersson@DESKTOP-NTG4L42>
2026-08-27 10:58:31 +0200

    merged from main

--- Infrastructure/Repositories/Placements/IPlacementRepository.cs
+++ Infrastructure/Repositories/Placements/IPlacementRepository.cs
@@ -5,6 +5,6 @@ namespace kanban_lia.Infrastructure.Repositories.Placements
     public interface IPlacementRepository
     {
         Task<Placement> CreateAsync(Placement placement);
-        Task<Placement?> GetByIdAsync(Guid id);
+        Task<Placement?> GetCurrentAsync(Guid id);
     }
 }
