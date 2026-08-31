77b1f8209e3804c6e0d53cc06e0474b7d1e4d880
David Lindberg <david.lindberg@informer.se>
2026-08-31 12:32:30 +0200

    fixed so that columnservicetst uses boardservice instead

--- kanban-lia.Integration.Tests/ColumnServiceTests.cs
+++ kanban-lia.Integration.Tests/ColumnServiceTests.cs
@@ -1,7 +1,7 @@
 ﻿using kanban_lia.Infrastructure.Repositories.Boards;
 using kanban_lia.Infrastructure.Repositories.Columns;
-using kanban_lia.Models.Domain.Boards;
 using kanban_lia.Models.Domain.Columns;
+using kanban_lia.Services.Boards;
 using kanban_lia.Services.Columns;
 using kanban_lia.Services.Columns.DTOs;
 using kanban_lia.Services.Columns.Exceptions;
@@ -12,30 +12,28 @@ namespace kanban_lia.Integration.Tests
     {
         private readonly DatabaseFixture _db = db;
 
-        private ColumnService CreateService()
+        private ColumnService CreateColumnService()
         {
             return new ColumnService(new ColumnRepository(_db.DbFactory));
         }
 
-        private async Task<BoardId> CreateBoardAsync()
+        private BoardService CreateBoardService()
         {
-            var boardRepo = new BoardRepository(_db.DbFactory);
-            var board = Board.Create("Test Board");
-            await boardRepo.CreateAsync(board);
-            return board.Id;
+            return new BoardService(new BoardRepository(_db.DbFactory));
         }
 
         [Fact]
         public async Task CreateAsync_WithValidId_PersistsColumnThatCanBeRetrieved()
         {
-            var service = CreateService();
+            var columnService = CreateColumnService();
+            var boardService = CreateBoardService();
 
-            var boardId = await CreateBoardAsync();
+            var boardId = await boardService.CreateAsync("Test Board");
             var columnId = new ColumnId(Guid.NewGuid());
 
-            await service.CreateAsync(new CreateColumnDto(columnId, "Test Column", 1, boardId));
+            await columnService.CreateAsync(new CreateColumnDto(columnId, "Test Column", 1, boardId));
 
-            var retrievedColumn = await service.GetByIdAsync(columnId);
+            var retrievedColumn = await columnService.GetByIdAsync(columnId);
 
             Assert.Equal("Test Column", retrievedColumn!.Title);
             Assert.Equal(columnId, retrievedColumn.Id);
@@ -44,14 +42,15 @@ namespace kanban_lia.Integration.Tests
         [Fact]
         public async Task GetByBoardIdAsync_ReturnsAllColumnsForBoard()
         {
-            var service = CreateService();
+            var columnService = CreateColumnService();
+            var boardService = CreateBoardService();
 
-            var boardId = await CreateBoardAsync();
+            var boardId = await boardService.CreateAsync("Test Board");
 
-            await service.CreateAsync(new CreateColumnDto(new ColumnId(Guid.NewGuid()), "Column 1", 1, boardId));
-            await service.CreateAsync(new CreateColumnDto(new ColumnId(Guid.NewGuid()), "Column 2", 2, boardId));
+            await columnService.CreateAsync(new CreateColumnDto(new ColumnId(Guid.NewGuid()), "Column 1", 1, boardId));
+            await columnService.CreateAsync(new CreateColumnDto(new ColumnId(Guid.NewGuid()), "Column 2", 2, boardId));
 
-            var columns = await service.GetByBoardIdAsync(boardId);
+            var columns = await columnService.GetByBoardIdAsync(boardId);
 
             Assert.Equal(2, columns.Count());
             Assert.Contains(columns, c => c.Title == "Column 1");
@@ -61,15 +60,16 @@ namespace kanban_lia.Integration.Tests
         [Fact]
         public async Task RenameAsync_WithValidId_UpdatesColumnTitle()
         {
-            var service = CreateService();
+            var columnService = CreateColumnService();
+            var boardService = CreateBoardService();
 
-            var boardId = await CreateBoardAsync();
+            var boardId = await boardService.CreateAsync("Test Board");
             var columnId = new ColumnId(Guid.NewGuid());
 
-            await service.CreateAsync(new CreateColumnDto(columnId, "Old Title", 1, boardId));
-            var renamed = await service.RenameAsync(new RenameColumnDto(columnId.Id, "New Title"));
+            await columnService.CreateAsync(new CreateColumnDto(columnId, "Old Title", 1, boardId));
+            var renamed = await columnService.RenameAsync(new RenameColumnDto(columnId.Id, "New Title"));
 
-            var updatedColumn = await service.GetByIdAsync(columnId);
+            var updatedColumn = await columnService.GetByIdAsync(columnId);
 
             Assert.True(renamed);
             Assert.Equal("New Title", updatedColumn!.Title);
@@ -78,16 +78,17 @@ namespace kanban_lia.Integration.Tests
         [Fact]
         public async Task DeleteAsync_DeletesColumn()
         {
-            var service = CreateService();
+            var columnService = CreateColumnService();
+            var boardService = CreateBoardService();
 
-            var boardId = await CreateBoardAsync();
+            var boardId = await boardService.CreateAsync("Test Board");
             var columnId = new ColumnId(Guid.NewGuid());
 
-            await service.CreateAsync(new CreateColumnDto(columnId, "Old Title", 1, boardId));
-            var deleted = await service.DeleteAsync(columnId);
+            await columnService.CreateAsync(new CreateColumnDto(columnId, "Old Title", 1, boardId));
+            var deleted = await columnService.DeleteAsync(columnId);
 
             Assert.True(deleted);
-            await Assert.ThrowsAsync<ColumnNotFoundException>(() => service.GetByIdAsync(columnId));
+            await Assert.ThrowsAsync<ColumnNotFoundException>(() => columnService.GetByIdAsync(columnId));
         }
     }
 }
