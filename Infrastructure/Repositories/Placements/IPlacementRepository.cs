28be9b4adb54cddeecb22c20fffbd65b59843bbd
felix.andersson <felix.andersson@DESKTOP-NTG4L42>
2026-09-07 10:32:26 +0200

    added BoardId as a parameter in getByColumn

--- Infrastructure/Repositories/Placements/IPlacementRepository.cs
+++ Infrastructure/Repositories/Placements/IPlacementRepository.cs
@@ -16,7 +16,7 @@ namespace kanban_lia.Infrastructure.Repositories.Placements
         Task CreateAsync(Placement placement);
         Task<IEnumerable<Placement>> GetCurrentAsync(IEnumerable<EntityId> entityIds, BoardId boardId);
         Task<IEnumerable<Placement>> GetCurrentByBoardAsync(BoardId boardId);
-        Task<IEnumerable<Placement>> GetCurrentByColumnAsync(ColumnId columnId);
+        Task<IEnumerable<Placement>> GetCurrentByColumnAsync(ColumnId columnId, BoardId boardId);
         Task<SortKeyRange> GetSortKeyRangeAsync(
             ColumnId columnId,
             SortKeyLookup lookup,
