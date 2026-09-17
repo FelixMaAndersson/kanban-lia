d0ac7c17ae9970ce005a54234330ecbe82eb82bf
felix.andersson <felix.andersson@DESKTOP-NTG4L42>
2026-09-17 09:47:25 +0200

    finished w basic CRUD for coluymn edge

--- Infrastructure/Repositories/Columns/IColumnEdgeRepository.cs
+++ Infrastructure/Repositories/Columns/IColumnEdgeRepository.cs
@@ -1,10 +1,12 @@
-﻿using kanban_lia.Models.Domain.Columns;
+﻿using kanban_lia.Models.Domain.Boards;
+using kanban_lia.Models.Domain.Columns;
 
 namespace kanban_lia.Infrastructure.Repositories.Columns
 {
     public interface IColumnEdgeRepository
     {
         Task CreateAsync(ColumnEdge columnEdge);
+        Task<IEnumerable<ColumnEdge>> GetByBoardIdAsync(BoardId boardId);
         Task<IEnumerable<ColumnEdge>> GetByFromColumnIdAsync(ColumnId fromColumnId);
         Task<IEnumerable<ColumnEdge>> GetByToColumnIdAsync(ColumnId toColumnId);
         Task<bool> DeleteAsync(ColumnEdge columnEdge);
--- Services/Columns/IColumnEdgeService.cs
+++ Services/Columns/IColumnEdgeService.cs
@@ -1,4 +1,6 @@
-﻿using kanban_lia.Models.Domain.Columns;
+﻿using kanban_lia.Models.Domain.Boards;
+using kanban_lia.Models.Domain.Columns;
+using kanban_lia.Models.Domain.Columns.DTOs;
 using kanban_lia.Services.Columns.DTOs;
 
 namespace kanban_lia.Services.Columns
@@ -6,8 +8,9 @@ namespace kanban_lia.Services.Columns
     public interface IColumnEdgeService
     {
         Task CreateAsync(CreateColumnEdgeDto dto);
-        Task<IEnumerable<ColumnEdge>> GetByFromColumnIdAsync(ColumnId fromColumnId);
-        Task<IEnumerable<ColumnEdge>> GetByToColumnIdAsync(ColumnId toColumnId);
+        Task<IEnumerable<ColumnEdgeDto>> GetByBoardIdAsync(BoardId boardId);
+        Task<IEnumerable<ColumnEdgeDto>> GetByFromColumnIdAsync(ColumnId fromColumnId);
+        Task<IEnumerable<ColumnEdgeDto>> GetByToColumnIdAsync(ColumnId toColumnId);
         Task<bool> DeleteAsync(ColumnEdge edge);
     }
 }
